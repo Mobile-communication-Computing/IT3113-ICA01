732bfe70a96efd1d035bc939dff2c222f929a7aa
piumal302 <piumal302@gmail.com>
2024-12-03 22:49:37 +0530

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,29 +1,30 @@
+import "../assets/CSS/layout.css";
+import { useState } from "react";
+import { flowers } from "./FlowerDB";
+import Product from "./Product";
 
-import '../assets/CSS/layout.css';
-
-
-export default function Products(){
-   
-    return(
-        <>
-            <div className="item1">
-                <h1>Flower Shop</h1>
-            </div>
-            <div className="item2">
-                <h4 className="card-title">Buy flowers</h4>
-                <div className="grid-container">
-                    {
-                        //product
-                    }
-                </div>
-
-            </div>
-            <div className="item3">
-                {
-                //cart
-                }
-            </div>
-        </>
-    );
-
+export default function Products() {
+  return (
+    <>
+      <div className="item1">
+        <h1>Flower Shop</h1>
+      </div>
+      <div className="item2">
+        <h4 className="card-title">Buy flowers</h4>
+        <div className="grid-container">
+          {
+            flowers.map((flower, index) => {
+              return <Product />;
+            })
+            //product
+          }
+        </div>
+      </div>
+      <div className="item3">
+        {
+          //cart
+        }
+      </div>
+    </>
+  );
 }
